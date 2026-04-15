331499fd6b7ea47b73a9f90a930245b29f96c3b0
Chetan AG <agchetan18@gmail.com>
2026-04-15 21:19:09 +0530

    Completed the Requirement 12: For Data Driven Testing with Database

--- src/test/java/com/database/DatabaseManager.java
+++ src/test/java/com/database/DatabaseManager.java
@@ -1,10 +1,7 @@
 package com.database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 import com.api.utils.ConfigManager;
 import com.zaxxer.hikari.HikariConfig;
