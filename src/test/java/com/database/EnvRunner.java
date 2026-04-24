ee1ba95e9507887dbb53fcce670a59d2ec9a5753
Chetan AG <agchetan18@gmail.com>
2026-04-24 13:05:03 +0530

    Added java-dotenv dependency to pom.xml file
    Created .env file and added it to .gitignore
    
    also practiced how to use java-dotenv dependency

---
+++ src/test/java/com/database/EnvRunner.java
@@ -0,0 +1,12 @@
+package com.database;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvRunner {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.out.println(dotenv.get("DB_USERNAME"));
+	}
+
+}
